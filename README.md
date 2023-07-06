d95e0fd3470c883198479c951f769f1a571b840e
anish bhattarai <anishji0306@gmail.com>
2023-07-06 11:08:24 +0545

    done

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Antivirious_in_python
-This is a sample antivirious made by anish.
-this antivirious takes two input till now.
-It can scan a given directory or scan all the files in a system.
-It scan based on the md5 hash.
-It also use the best yara rule from  https://github.com/Yara-Rules/rules and scanned all the file based on all the yara rule.
+## This is a sample antivirious made by anish.
+`this antivirious takes three input.`
+1. Scan full system.
+2. Scan the give directory.
+3. Live manitering the given directory.
 
+## What does it do?
+The system includes a database with MD5 hashes of known malware. It functions by computing each file's hash and comparing it to the database. When a suspicious file is found, the user is asked if they want to delete it. The system also makes use of a sizable collection of modern and sophisticated YARA rules, sourced from https://github.com/Yara-Rules/rules and numbering over 1000 rules. It checks files for compliance with these rules and alerts the user to any suspicious findings so they can choose whether to delete the file or not. 
+Furthermore, the system uses watchdog library functionality for live monitoring, which keeps track of file system events like creations, modifications, and deletions within a given directory. If any suspicious activity is found, the system investigates it using hash and rule checks and gives the user pertinent information. All events are logged into a file in order to keep track of runtime activities. The system makes effective use of a Bloom Filter data structure to find malware hashes. A Bloom Filter is a probabilistic data structure for representing a set of elements that makes use of a bit array and numerous hashing operations.
+
+## How to use it.
+```
+git clone https://github.com/Anishsir404/Antivirious_in_python.git
+```
+```
+cd Antivirious_in_python
+git clone https://github.com/Yara-Rules/rules.git
+```
+```
+pip -i requirement.txt
+```
+```
+python main.py
+```
--- main.py
+++ main.py
@@ -4,11 +4,33 @@ import hashlib
 import concurrent.futures
 from pybloom_live import BloomFilter
 import datetime
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 # Constants
 MAX_THREADS = 8  # Maximum number of threads to use for scanning
 MAX_FILE_SIZE = 10 * 1024 * 1024  # Maximum file size to scan in bytes (10 MB)
+# Global variables
+yara_rules = None
+malware_bloom_filter = None
+log_file = None
 
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        super(FileEventHandler, self).__init__()
+
+    def on_created(self, event):
+        if not event.is_directory:
+            file_path = event.src_path
+            live_scan_file(file_path)
+            log_scan_result(file_path)
+
+    def on_modified(self, event):
+        if not event.is_directory:
+            file_path = event.src_path
+            live_scan_file(file_path)
+            log_scan_result(file_path)
 def load_yara_rules(rule_dir):
     try:
         rule_files = {file: os.path.join(rule_dir, file) for file in os.listdir(rule_dir) if file.endswith('.yar')}
@@ -115,6 +137,78 @@ def scan_directory(directory_path, yara_rules, malware_bloom_filter):
 
     print(f"Scan results written to log file: {log_file_path}")
 
+def live_scan_file(file_path):
+    global yara_rules, malware_bloom_filter
+    if yara_rules is None or malware_bloom_filter is None:
+        print("YARA rules or malware bloom filter not initialized.")
+        return
+
+    try:
+        if os.path.getsize(file_path) <= MAX_FILE_SIZE:
+            matches = yara_rules.match(filepath=file_path)
+            md5_hash = calculate_md5(file_path)
+            if matches or md5_hash in malware_bloom_filter:
+                print(f"File: {file_path}")
+                if matches:
+                    for match in matches:
+                        print(f"Rule: {match.rule}\nMetadata: {match.meta}")
+                if md5_hash in malware_bloom_filter:
+                    print("Detected as malware (based on MD5 hash)")
+                print("-----------------------")
+
+    except Exception as e:
+        print(f"Error scanning file {file_path}: {e}")
+
+def log_scan_result(file_path):
+    global log_file
+    if log_file is None:
+        print("Log file not initialized.")
+        return
+
+    try:
+        if os.path.getsize(file_path) <= MAX_FILE_SIZE:
+            matches = yara_rules.match(filepath=file_path)
+            md5_hash = calculate_md5(file_path)
+            if matches or md5_hash in malware_bloom_filter:
+                log_file.write(f"File: {file_path}\n")
+                if matches:
+                    for match in matches:
+                        log_file.write(f"Rule: {match.rule}\nMetadata: {match.meta}\n")
+                if md5_hash in malware_bloom_filter:
+                    log_file.write("Detected as malware (based on MD5 hash)\n")
+                log_file.write("-----------------------\n")
+                log_file.flush()
+
+    except Exception as e:
+        print(f"Error logging scan result for file {file_path}: {e}")
+
+def start_monitoring(directory_path):
+    global yara_rules, malware_bloom_filter, log_file
+    yara_rules = load_yara_rules(rule_dir)
+    malware_bloom_filter = create_malware_bloom_filter(malware_hash_file)
+
+    if yara_rules is None:
+        print("Failed to load YARA rules.")
+        return
+    
+    log_filename = f"live_monitering_{time.strftime('%Y%m%d%H%M%S')}.log"
+    log_file = open(log_filename, 'w')
+
+    event_handler = FileEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, directory_path, recursive=True)
+    observer.start()
+
+    try:
+        print(f"Monitoring directory: {directory_path}")
+        print("Press Ctrl+C to stop monitoring.")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
+    log_file.close()
 # Example usage
 while True:
     rule_dir = "D:\\3rd-sem\\programming\\project\\antivirus\\rules"
@@ -124,9 +218,10 @@ while True:
     print("Antivirus Scanner")
     print("1. Scan All Files")
     print("2. Scan Directory")
-    print("3. Exit")
+    print("3. Live Monitoring")
+    print("4. Exit")
 
-    choice = input("Enter your choice (1-3): ")
+    choice = input("Enter your choice (1-4): ")
 
     if choice == '1':
         scan_directory('/')
@@ -134,8 +229,11 @@ while True:
         directory_path = input("Enter the directory path to scan: ")
         scan_directory(directory_path, yara_rules, malware_bloom_filter)
     elif choice == '3':
-        break
+        directory_path = input("Enter the directory path to monitor: ")
+        start_monitoring(directory_path)
+    elif choice == '4':
+        print("Exiting...")
+        exit()
     else:
         print("Invalid choice. Please try again.")
 
-print("Exiting...")
